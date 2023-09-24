5ea1bec13ee69566860236080121ab2c3105a501
omta-coder <76040920+omta-coder@users.noreply.github.com>
2023-09-24 09:06:35 +0530

    MULTIPLE HOVERING

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var main = document.querySelector("#main")
-var crsr = document.querySelector(".cursor")
+var elem = document.querySelectorAll(".elem");
 
-main.addEventListener("mousemove",function(dets){
-    crsr.style.left = dets.x+"px"
-    crsr.style.top = dets.y+"px"
-})
+elem.forEach(function(val){
+   
+    
+    val.addEventListener("mouseenter",function(){
+        val.childNodes[3].style.opacity= 1
+    });
+    
+    val.addEventListener("mouseleave",function(){
+        val.childNodes[3].style.opacity= 0
+    });
+
+    val.addEventListener("mousemove",function(dets){
+        val.childNodes[3].style.left= dets.x+"px"
+    });
+
+
+});
