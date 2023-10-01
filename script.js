7489767827f704e77e777e188f7a1d19bfedd817
omta-coder <76040920+omta-coder@users.noreply.github.com>
2023-10-01 09:52:38 +0530

    SCROLLTRIGGER

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-gsap.from("#page1 #box",{
-    scale:0,
-    duration:2,
-    delay:1,
-    rotate:360
-})
-
-gsap.from("#page2 #box",{
-    scale:0,
-    duration:2,
-    rotate:360,
+gsap.to("#page2 img",{
+    width:"80%",
     scrollTrigger:{
-        trigger:"#page2 #box",
+        trigger:"#page2",
         scroller:"body",
         markers:true,
-        start:"top 60%",
-        end:"top 30%",
+        start:"top 0",
+        end:"top -50%",
         scrub:2,
-        //pin:true
+        pin:true
     }
 })
