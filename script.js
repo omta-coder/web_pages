b90fb8f110126ae95c8a8378871214b5f18394cf
omta-coder <76040920+omta-coder@users.noreply.github.com>
2023-10-02 08:04:04 +0530

    DAVIS

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-gsap.to("#page2 img",{
-    width:"80%",
-    scrollTrigger:{
-        trigger:"#page2",
-        scroller:"body",
-        markers:true,
-        start:"top 0",
-        end:"top -50%",
-        scrub:2,
-        pin:true
-    }
+var tl = gsap.timeline()
+
+tl.from("nav img,nav h4",{
+    y:-50,
+    opacity:0,
+    duration:1,
+    stagger:.5
+})
+tl.from("h1",{
+    opacity:0,
+    x:-100,
+    duration:1,
+    stagger:.4
+})
+tl.from("#right img",{
+    opacity:0,
+    x:100,
+    duration:1,
+    rotateY:90
+})
+tl.from("button",{
+    opacity:0,
+    y:50,
+    duration:1,
 })
