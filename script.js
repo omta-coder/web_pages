bab11703a666ae53d5f0ab1f1f93c631d9816e38
omta-coder <76040920+omta-coder@users.noreply.github.com>
2023-10-03 07:36:32 +0530

    GSAP PROJECT

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-var tl = gsap.timeline()
 
-tl.from("nav img,nav h4",{
-    y:-50,
-    opacity:0,
+var tl = gsap.timeline({
+    repeat:-1
+});
+
+tl.to(".imagecontainer",{
+    width:"100%",
+    ease:Expo.easeInOut,
     duration:1,
-    stagger:.5
-})
-tl.from("h1",{
-    opacity:0,
-    x:-100,
+    stagger:2
+},'a')
+tl.to(".text h1",{
+    ease:Expo.easeInOut,
+    stagger:2,
     duration:1,
-    stagger:.4
-})
-tl.from("#right img",{
-    opacity:0,
-    x:100,
-    duration:1,
-    rotateY:90
-})
-tl.from("button",{
-    opacity:0,
-    y:50,
-    duration:1,
-})
+    top:0
+},'a')
+tl.to(".text h1",{
+    delay:2,
+    ease:Expo.easeInOut,
+    stagger:2,
+    top:"-100%"
+},'a')
