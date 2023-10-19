2e5334ae7157b07ce470b88e19b9653dd09530bb
omta-coder <76040920+omta-coder@users.noreply.github.com>
2023-10-19 07:19:54 +0530

    DR PEPPER WITH GSAP

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-gsap.from("#nav",{
-    x:"100%",
+
+var tl = gsap.timeline()
+
+tl.from(" #nav,#nav h4,#nav img ,#nav i",{
+    y:-50,
+    duration:0.5,
+    delay:0.5,
+    opacity:0,
+    stagger:0.3
+})
+tl.from("#parent,h1,p",{
+    x:-50,
+    opacity:0,
+    stagger:.3
+})
+tl.from("#right img",{
+    rotate:360,
+    duration:1,
+    stagger:0.8,
+    x:400,
     opacity:0
 })
-gsap.from("#nav i",{
-    y:"-100%",
-    stagger:0.5,
-    opacity:0
+tl.from("#oval",{
+    y:50,
+    opacity:0,
+    stagger:.3
 })
-var tl =gsap.timeline(
-    {
-        repeat:-1
-    }
-)
-tl.to("#center-image",{
-    width:"100%",
-    duration:2,
-    stagger:2
-},'a')
+
+
+var main = document.querySelector("#main")
+var crsr = document.querySelector(".cursor")
+
+main.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+})
