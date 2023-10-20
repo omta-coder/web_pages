702779c3d3e4b97f90c29341b1c232c33cc60898
omta-coder <76040920+omta-coder@users.noreply.github.com>
2023-10-20 07:32:29 +0530

    FOOD ORDER

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
+var main = document.querySelector("#main")
+var img = document.querySelector("#main img")
+
+main.addEventListener("mouseenter",function(){
+    img.style.height=`500px`
+    img.style.width=`600px`
+})
+main.addEventListener("mouseleave",function(){
+    img.style.height=`450px`
+})
 
 var tl = gsap.timeline()
-
-tl.from(" #nav,#nav h4,#nav img ,#nav i",{
-    y:-50,
-    duration:0.5,
-    delay:0.5,
-    opacity:0,
-    stagger:0.3
+tl.from("#nav h4,#nav i",{
+    y:-100,
+    scale:0,
+    duration:1,
+    stagger:.8
 })
-tl.from("#parent,h1,p",{
+tl.from("h1,p,button",{
     x:-50,
     opacity:0,
-    stagger:.3
+    duration:.6,
+    stagger:.8
 })
-tl.from("#right img",{
-    rotate:360,
-    duration:1,
-    stagger:0.8,
-    x:400,
-    opacity:0
-})
-tl.from("#oval",{
-    y:50,
-    opacity:0,
-    stagger:.3
-})
-
-
-var main = document.querySelector("#main")
-var crsr = document.querySelector(".cursor")
-
-main.addEventListener("mousemove",function(dets){
-    crsr.style.left = dets.x+"px"
-    crsr.style.top = dets.y+"px"
+tl.from("#main img",{
+    x:50,
+    scale:0,
+    duration:.6,
 })
