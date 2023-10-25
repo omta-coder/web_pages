10ea7030b81c019aecc7048218143b849b4bdd72
omta-coder <76040920+omta-coder@users.noreply.github.com>
2023-10-25 09:06:12 +0530

    MALDIVES LANDING PAGE

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 var tl = gsap.timeline()
-tl.from("#nav img,#nav h4",{
+tl.from("#nav h4",{
     y:-50,
     opacity:0,
-    
+    duration:.8,
+    stagger:.2
 })
-tl.from("#left h1,#left p",{
+tl.from("#center h1,#center p,button",{
     x:-100,
     opacity:0,
-    stagger:.5
+    stagger:.2
 })
-tl.from("#photo img",{
+tl.from("#right img",{
     x:100,
-    scale:0
-})
-tl.from("#footer,#box",{
-    y:100,
     opacity:0,
-    stagger:.5
+})
+
+gsap.to("#page2 h1",{
+    transform:`translateX(-280%)`,
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"body",
+        strat:"top 0",
+        end:"top -150%",
+        scrub:2,
+        pin:true
+    }
+})
+
+gsap.to("#page3 img",{
+    width:`100%`,
+    scrollTrigger:{
+        trigger:"#page3",
+        scroller:"body",
+        strat:"top 30%",
+        end:"top -60%",
+        scrub:2,
+        pin:true
+    }
 })
