629a42c8ea4b33cc573ef84536be60fabcc01d1a
omta-coder <76040920+omta-coder@users.noreply.github.com>
2023-11-04 12:57:02 +0530

    URBAN

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-var tl = gsap.timeline()
-tl.from("#nav h4",{
+
+
+var tl = gsap.timeline();
+
+tl.from("#nav,h2,h5,h4",{
     y:-50,
     opacity:0,
-    duration:.8,
-    stagger:.2
+    duration:1,
+    stagger:.5
 })
-tl.from("#center h1,#center p,button",{
-    x:-100,
+tl.from("button,h1,p",{
     opacity:0,
-    stagger:.2
+    x:-100,
+    duration:1,
+    stagger:.5
 })
 tl.from("#right img",{
-    x:100,
-    opacity:0,
-})
-
-gsap.to("#page2 h1",{
-    transform:`translateX(-280%)`,
-    scrollTrigger:{
-        trigger:"#page2",
-        scroller:"body",
-        strat:"top 0",
-        end:"top -150%",
-        scrub:2,
-        pin:true
-    }
-})
-
-gsap.to("#page3 img",{
-    width:`100%`,
-    scrollTrigger:{
-        trigger:"#page3",
-        scroller:"body",
-        strat:"top 30%",
-        end:"top -60%",
-        scrub:2,
-        pin:true
-    }
+    x:`100%`,
+    duration:1,
 })
